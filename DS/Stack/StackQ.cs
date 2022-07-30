cef7fba4b2628f093390601a7ba038e5cb7336ba
Saket <Saket@DESKTOP-HBLD4O2>
2022-07-30 19:10:50 +0530

    PrintNearestlargestOnLeft

--- DS/Stack/StackQ.cs
+++ DS/Stack/StackQ.cs
@@ -7,7 +7,6 @@ namespace DS.Stack
     internal class StackQ
     {
         //Find nearest smallest element on left
-
         public void PrintNearestSmallestOnLeft(int[] a)
         {
             Stack<int> vs = new Stack<int>();
@@ -21,6 +20,32 @@ namespace DS.Stack
                         vs.Pop();
                     }
                     else {
+                        Console.WriteLine("Nearest smallest of {0} on left is {1}", a[i], vs.Peek());
+                        break;
+                    }
+                }
+                if (vs.Count == 0)
+                {
+                    Console.WriteLine("Nearest smallest of {0} on left is {1}", a[i], -1);
+                }
+                vs.Push(a[i]);
+            }
+        }
+
+        public void PrintNearestlargestOnLeft(int[] a)
+        {
+            Stack<int> vs = new Stack<int>();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                while (vs.Count != 0)
+                {
+                    if (vs.Peek() < a[i])
+                    {
+                        vs.Pop();
+                    }
+                    else
+                    {
                         Console.WriteLine("Nearest smallest of {0} on left is {1}", a[i], vs.Peek());
                         break;
                     }
